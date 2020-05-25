840c3daabded985508040a27478b82d13fc0ea91
Daniel Kim <berzzubunny@gmail.com>
2020-05-26 01:12:45 +0900

    docs : 2005025 python online class TIL

--- practice3.py
+++ practice3.py
@@ -1,27 +1,11 @@
-class Language:
-    default_language = "English"
+N = int(input())
 
-    def __init__(self):
-        self.show = '나의 언어는' + self.default_language
-
-    def print_language(self):
-        print(self.show)
-
-    @classmethod # 일본어 > English > 일본어
-    def class_my_language(cls):
-        return cls()
+for i in range(N):
+    n = int(input())
+    if n == 0:
+        del lst[-1]
+    else:
+        lst.append(n)
+        
     
-    @staticmethod # 일본어 > English, 부모클래스(Language) 의 값을 가져옴
-    def static_my_language():
-        return Language()
-
-
-
-class KoreanLanguage(Language):
-    default_language = "일본어"
-
-a = Language.static_my_language()
-a.print_language()
-
-b = Language.class_my_language()
-b.print_language()
+print(sum(lst))
